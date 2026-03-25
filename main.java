2c529080969a3bbe48b923bea33627cda1e40c1f
husna-zalakhan <husna.zalakhan@gmail.com>
2026-03-25 01:06:44 +0000

    parameters and arguments

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 package practices.java_mosh_;
 
+import java.awt.Point;
 import java.util.Date;
 
 public class main {
@@ -11,23 +12,60 @@ public class main {
     // variables: used to temporally store data in computer's memiry.
     //primitive data type don't have members
     byte age = 20;
+    /*age---->name, label, identifier, identify our variable */
+    System.out.println(age);
+
     long viewsCount= 3_221_442_334L;
     float price = 10.99F;
     char letter = 'A';
     String app = "ddf"; 
-    boolean isEligible= false;
+    //boolean isEligible= false;
 
     //Refrence type
+    //current Date
     Date now = new Date(); //green Date is instance of the date class. 
     //blue Date is reference type, we need to allocate memory.  
     
     //sout ----press tap, shortcut for System.out....
     System.out.println(now);
 
-    /*age---->name, label, identifier, identify our variable */
-    System.out.println(age);
+    byte x= 1;
+    byte y= x;
+    x=2;
+    System.out.println(y);
+
+    Point point1= new Point(1,1);
+    Point point2= point1;
+    point1.x =2;
+    System.out.println(point2);
+
+    String message = "Hello world" + "!!!";
+    
+    System.out.println(message.endsWith("!!"));
+    System.out.println(message.startsWith("!!"));
+    System.out.println(message.length());
+    System.out.println(message.indexOf("z"));
+    System.out.println(message.indexOf("H"));
+    System.out.println(message.indexOf("e"));
+    System.out.println(message.replace("!!", "sky" )); 
+    /*has 2 parameters 
+    target and replaceent are parameter
+    !! and sky are arguments
+    parameters
+        are the holes we define in our methods
+        arguments
+        are the actual value that we pass to these methods 
+    */
 
 
+
+
+
+
+
+
+
+    
 
 }
 }
