046197f6fed62ec71ede14bc035f8fca496599e9
husna-zalakhan <husna.zalakhan@gmail.com>
2026-03-27 04:57:24 +0000

    corrected the indentation

--- main.java
+++ main.java
@@ -108,120 +108,120 @@ public class main {
     System.out.println(Arrays.deepToString(priceNum));
 
     //Constants
-   final float PI = 3.14F; // final change it to constant
+    final float PI = 3.14F; // final change it to constant
      
-  //Arithmetic Expressions
-  // + - * / %
-  int result = 10+3;
-  System.out.println(result);
+    //Arithmetic Expressions
+    // + - * / %
+    int result = 10+3;
+    System.out.println(result);
 
-  int results = 10/3;//in java the division of two whole number, is a whole number.
-  System.out.println(results);
+    int results = 10/3;//in java the division of two whole number, is a whole number.
+    System.out.println(results);
 
-  double finalResult = (double)10 / (double)3;
-  System.out.println(finalResult);
+    double finalResult = (double)10 / (double)3;
+    System.out.println(finalResult);
 
-  // increment operator
-  int z = 31;
-  //postfix
-  int h = ++z;
-  System.out.println(z);
-  System.out.println(h);
-  //++z prefix
-  //z++ postfix
+    // increment operator
+    int z = 31;
+    //postfix
+    int h = ++z;
+    System.out.println(z);
+    System.out.println(h);
+    //++z prefix
+    //z++ postfix
 
-  int u = 3;
-  u =  u + 2;
-  u += 5; //compound assignment operator, augmented operators
-  System.out.println(u);
+    int u = 3;
+    u =  u + 2;
+    u += 5; //compound assignment operator, augmented operators
+    System.out.println(u);
 
-  /*order and operation
-  ()
-  * /
-  + -
-  */
-  int r = 10 + 3 * 2;
-  System.out.println(r);
+    /*order and operation
+    ()
+    * /
+    + -
+    */
+    int r = 10 + 3 * 2;
+    System.out.println(r);
   
-  int w = (10 + 3) * 2;
-  System.out.println(w);
+    int w = (10 + 3) * 2;
+    System.out.println(w);
 
-  //casting
-  // implicit casting, automatic casting
-  // byte > short > int > long > float > double
-  // byte can be automtically converted to short, short to int, int to long
-  short e = 1;
-  int f = e + 2;
-  System.out.println(f);
+    //casting
+    // implicit casting, automatic casting
+    // byte > short > int > long > float > double
+    // byte can be automtically converted to short, short to int, int to long
+    short e = 1;
+    int f = e + 2;
+    System.out.println(f);
 
-  double a = 1.1; 
-  double b = a + 2;
-  System.out.println(b);
+    double a = 1.1; 
+    double b = a + 2;
+    System.out.println(b);
 
-  int v = (int)a + 2;//explicit casting, we convert "a" to an int
+    int v = (int)a + 2;//explicit casting, we convert "a" to an int
   
-  // we can't cast a string to a number
+    // we can't cast a string to a number
 
-  String c = "1";
-  int d = Integer.parseInt(c) + 2;
-  System.out.println(d);
+    String c = "1";
+    int d = Integer.parseInt(c) + 2;
+    System.out.println(d);
   
-  String i = "34";
-  Integer.parseInt(i); //integer is class, parseInt is method
-  //this method take string and return integer
+    String i = "34";
+    Integer.parseInt(i); //integer is class, parseInt is method
+    //this method take string and return integer
   
-  String g= "23";
-  Short.parseShort(g);
-  //takes a string and return short
+    String g= "23";
+    Short.parseShort(g);
+    //takes a string and return short
 
-  // The Math Class
-  //Round: we can round a floating value to  whole number
-  int intResult = Math.round(1.2F); 
-  System.out.println(intResult);
+    // The Math Class
+    //Round: we can round a floating value to  whole number
+    int intResult = Math.round(1.2F); 
+    System.out.println(intResult);
   
-  /*Ceil: it is a method which return the smallest integer
-  that is greater than or equal to this number so the ceilling of 
-  1.1 is 2 */
-  int L = (int)Math.ceil(1.0002F);
-  System.out.println(L);
+    /*Ceil: it is a method which return the smallest integer
+    that is greater than or equal to this number so the ceilling of 
+    1.1 is 2 */
+    int L = (int)Math.ceil(1.0002F);
+    System.out.println(L);
 
-  int j = (int)Math.floor(3.3F); //te floor of a number is the largest integer that is the smaller or
-  //equal to this number 
-  System.out.println(j);
+    int j = (int)Math.floor(3.3F); //te floor of a number is the largest integer that is the smaller or
+    //equal to this number 
+    System.out.println(j);
 
-  int k = Math.max(4, 5);
-  System.out.println(k);
-  //max: return the greater of two values
+    int k = Math.max(4, 5);
+    System.out.println(k);
+    //max: return the greater of two values
 
-  int m = Math.min(7, 8);
-  System.out.println(m);
-  //min: return the smallest of two values
+    int m = Math.min(7, 8);
+    System.out.println(m);
+    //min: return the smallest of two values
 
-  //Random: is generating a random value between 0 and 1
-  //this method return double
-  double n = Math.random();
-  System.out.println(n);// print a floating value between 0 and 1
+    //Random: is generating a random value between 0 and 1
+    //this method return double
+    double n = Math.random();
+    System.out.println(n);// print a floating value between 0 and 1
 
-  // a number between 0 and 100
-  double o = Math.random() * 100;
-  //the method ,ath.random() doesn't take any argument
-  System.out.println(Math.round(Math.random() * 100));
-  /* if we want an integer result
-  int o = (int) (Math.random()* 100); */
+    // a number between 0 and 100
+    double o = Math.random() * 100;
+    //the method ,ath.random() doesn't take any argument
+    System.out.println(Math.round(Math.random() * 100));
+    /* if we want an integer result
+    int o = (int) (Math.random()* 100); */
 
-  //Formatting Numbers
-  //Number Format class
+    //Formatting Numbers
+    //Number Format class
 
-  // NumberFormat currency = new NumberFormat(); -----> Error
-  // this will give error because Number format class is abstract, it can't be instantiated 
-  // in java we have abstract classes, we can't create new operator to create instance of them
+    // NumberFormat currency = new NumberFormat(); -----> Error
+    // this will give error because Number format class is abstract, it can't be instantiated 
+    // in java we have abstract classes, we can't create new operator to create instance of them
 
-  NumberFormat currency= NumberFormat.getCurrencyInstance();
-  /*this class will create an instance of the number formt class and return it 
-  so instead of using the "new" operator, we use this method and this is what we called a factory method.
-  Factory Method: it is like a factory, it creates new objcts. */
-  currency.format(2424555.354);
-  
+    NumberFormat currency= NumberFormat.getCurrencyInstance();
+    /*this class will create an instance of the number formt class and return it 
+    so instead of using the "new" operator, we use this method and this is what we called a factory method.
+    Factory Method: it is like a factory, it creates new objcts. */
+    String p = currency.format(2424555.354);
+    System.out.println(p);
 
   
   
